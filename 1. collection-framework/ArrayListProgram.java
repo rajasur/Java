c3a8e2da34b078fd75740e08c901fb6b7deb3581
I571352 <raja.sur@sap.com>
2023-08-28 17:14:20 +0530

    Update arrylist program.

--- 1. collection-framework/ArrayListProgram.java
+++ 1. collection-framework/ArrayListProgram.java
@@ -4,7 +4,7 @@ public class ArrayListProgram {
     public static void main(String args[]){
         ArrayList al=new ArrayList<>();
         al.add(10);
-        al.add("Raja S");
+        al.add("Raja S2");
         al.add("SAP");
         System.out.println(al);
     }
