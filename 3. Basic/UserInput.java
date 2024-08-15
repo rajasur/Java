797d331e98053aad2431990d972015b9612572d7
Raja's PC <colab.raja@gmail.com>
2024-08-15 11:27:19 +0530

    update

--- 3. Basic/UserInput.java
+++ 3. Basic/UserInput.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 public class UserInput {
     public static void main(String[] args) {
-        Scanner input= new Scanner(System.in);
+        Scanner sc= new Scanner(System.in);
         System.out.print("Please enter your name: ");
-        String name=input.nextLine();
+        String name=sc.nextLine();
         System.out.println("Good Morning " + name);
         System.out.print("Also tell me your age: ");
-        int age=input.nextInt();
+        int age=sc.nextInt();
         System.out.println("Your age is: " + age);
+        sc.close();
     }
 }
